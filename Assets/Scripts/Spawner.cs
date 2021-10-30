8ea745acdc2c09b0a1897e37f790b75e98b261d0
funwareDev <kosyk.nazar@gmail.com>
2021-10-30 13:57:39 +0300

    Added spawning enemies in certain radius.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,30 +7,53 @@ public class Spawner: MonoBehaviour
     [SerializeField] private Transform _spawnCenter;
     [SerializeField] private GameObject[] _spawnObjects;
 
-    [SerializeField] private float _spawnCooldown;
-
     [SerializeField] private float _spawnRadiusStart;
     [SerializeField] private float _spawnRadiusEnd;
 
-    [SerializeField] private int _spawnEntitiesCount;
+    [SerializeField] private int _maxEntitiesCount = 3;
+    [SerializeField] private int _currentEntitiesCount;
 
+    [SerializeField] private float _spawnCooldown = 1f;
 
     private void Awake()
     {
+
     }
 
     private void Start()
     {
-        
+        InvokeRepeating(nameof(Spawn), 0, _spawnCooldown);
     }
 
-    private void Spawn(Vector2 spawnPoint, int indexOfObject)
+    private void SpawnAtThePoint(Vector2 spawnPoint, int indexOfObject)
     {
         Instantiate(_spawnObjects[indexOfObject], spawnPoint, Quaternion.identity);
     }
 
     private void Spawn() 
     {
+        if (_currentEntitiesCount >= _maxEntitiesCount) return;
 
+        float x = UnityEngine.Random.Range(_spawnRadiusStart, _spawnRadiusEnd);
+        float y = UnityEngine.Random.Range(_spawnRadiusStart, _spawnRadiusEnd);
+
+        Vector3 point = new Vector3(x, y);
+        Vector3 direction = point - _spawnCenter.position;
+
+        point = Quaternion.Euler(0, 0, UnityEngine.Random.Range(0, 360)) * direction;
+
+        SpawnAtThePoint(point, 0);
+        _currentEntitiesCount++;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Drawing start radius
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(_spawnCenter.position, _spawnRadiusStart);
+
+        // Drawing start radius
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_spawnCenter.position, _spawnRadiusEnd);
     }
 }
