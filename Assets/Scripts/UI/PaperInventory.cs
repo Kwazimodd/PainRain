80a2b516b2e93f5a0a10d47ae2aec321c2ffd449
funwareDev <kosyk.nazar@gmail.com>
2021-10-30 23:10:59 +0300

    Started Work

---
+++ Assets/Scripts/UI/PaperInventory.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperInventory : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
