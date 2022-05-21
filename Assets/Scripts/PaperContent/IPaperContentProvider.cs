5ef4de7772f737497067b1755f0c28d32faad3b9
funware <kosyk.nazar@gmail.com>
2022-05-21 20:48:14 +0300

    Work on game facade and finishited paper content proxy.

---
+++ Assets/Scripts/PaperContent/IPaperContentProvider.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IPaperContentProvider
+{
+    List<String> GetContent();
+}
--- Assets/Scripts/Scriptable Objects/PaperContentScriptableObject.cs
+++ Assets/Scripts/Scriptable Objects/PaperContentScriptableObject.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
-public class PaperContentScriptableObject : ScriptableObject
+public class PaperContentScriptableObject : ScriptableObject, IPaperContentProvider
 {
     public List<String> Content;
+
+    public List<String> GetContent()
+    {
+        return Content;
+    }
 }
