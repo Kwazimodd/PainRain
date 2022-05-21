50dc2f26393678d06b052001569455dc8c6212a2
funware <kosyk.nazar@gmail.com>
2022-05-21 20:48:14 +0300

    Work on game facade and finishited paper content proxy.

---
+++ Assets/Scripts/PaperContent/PaperContent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+class PaperContent : IPaperContentProvider
+{
+    public List<String> Content = new List<string>();
+    public int ContentCount { get; set; } = 9;
+
+    public List<string> GetContent()
+    {
+        return Content;
+    }
+
+    public void ParseData(string from)
+    {
+        using (var webClient = new System.Net.WebClient())
+        {
+            for (int i = 0; i < ContentCount; i++)
+            {
+                var json = webClient.DownloadString(from);
+                //dynamic stuff = JObject.Parse(json);
+                //var value = stuff.message;
+                Content.Add(json);
+            }
+        }
+        //https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1
+    }
+}
