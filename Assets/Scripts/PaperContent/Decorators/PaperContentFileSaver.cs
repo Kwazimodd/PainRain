f41f4b118c5e0c3c44cc338409342a0814d02ad1
funware <kosyk.nazar@gmail.com>
2022-05-22 17:59:33 +0300

    Finishited decorator

---
+++ Assets/Scripts/PaperContent/Decorators/PaperContentFileSaver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+/// <summary>
+/// Use with PaperContentProxy or simple PaperContent
+/// </summary>
+class PaperContentFileSaver : PaperContentDecorator
+{
+    public static string FileName { get; set; } = "C:\\Users\\User\\Documents\\Unity\\PainRain\\Assets\\papercontent.txt";
+    public static bool IsFileExists => File.Exists(FileName);
+    public PaperContentFileSaver(IPaperContentProvider contentProvider) : base(contentProvider)
+    {
+
+    }
+
+    public override List<string> GetContent()
+    {
+        var content = PaperContentProvider.GetContent();
+        if (!IsFileExists)
+        {
+            var stream = File.Create(FileName);
+            var streamtWriter = new StreamWriter(stream);
+
+            string buff = "";
+            foreach (var line in content)
+            {
+                buff += line + "\n";
+            }
+            streamtWriter.Write(buff);
+            streamtWriter.Close();
+        }
+        return content;
+    }
+
+}
---
+++ Assets/Scripts/PaperContent/PaperContentFromFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class PaperContentFromFile : IPaperContentProvider
+{
+    public List<string> Content { get; set; } = new List<string>();
+    public string FileName { get; set; } = PaperContentFileSaver.FileName;
+    public List<string> GetContent()
+    {
+        if (Content.Count == 0)
+        {
+            var buff = File.ReadAllText(FileName);
+            var lines = buff.Split('\n');
+
+            foreach (var line in lines)
+            {
+                Content.Add(line);
+            }
+        }
+        return Content;
+    }
+}
+
