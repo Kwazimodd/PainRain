c15a5f849f9f934e8ee841b83603a9eb4ce5bba1
funware <kosyk.nazar@gmail.com>
2022-05-22 17:59:33 +0300

    Finishited decorator

---
+++ Assets/Scripts/PaperContent/Decorators/PaperContentDecorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+abstract class PaperContentDecorator : IPaperContentProvider
+{
+    public IPaperContentProvider PaperContentProvider { get; set; }
+
+    public PaperContentDecorator(IPaperContentProvider contentProvider)
+    {
+        PaperContentProvider = contentProvider;
+    }
+    public virtual List<string> GetContent()
+    {
+        return PaperContentProvider.GetContent();
+    }
+}
